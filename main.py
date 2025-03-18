fad7832488da71ba571e6096b2126c2429d1191c
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-18 15:47:32 -0300

    add include_router

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
+import end_points
 
 app = FastAPI(title='Inflation Rate Adjust API',
               version='0.0.1')
 
-@app.get("/brl_inflation_adjust")
-def brl_inflation_adjust(data:Request):
+app.include_router(end_points.router, tags=['Recipe Finder'])
 
-  
-    return {"message": "Olá, FastAPI!"}
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="127.0.0.1", port=8000)
