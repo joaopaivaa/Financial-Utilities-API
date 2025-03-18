d174e01e5fffe867b86a7e04ac9f6c7fbe798454
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-18 15:48:53 -0300

    fix request name

--- base_model.py
+++ base_model.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
-class IngredientsRequest(BaseModel):
+class InflationRequest(BaseModel):
     dates: str = Field(description='Dates', default=None)
     values: float = Field(description='Values', default=None)
     present_date: str = Field(description='Present Date', default=None)
