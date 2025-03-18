0b45bd23c606d590f9087d7dc62f997a04b6edd5
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-18 15:48:07 -0300

    Update adjust_inflation_functions.py

--- adjust_inflation_functions.py
+++ adjust_inflation_functions.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-def adjust_BRL_inflation(dates:str, values:float, present_date:str=df_daily_ipca['Date'].max()):
+def adjust_BRL_inflation(dates:str, values:float, present_date:str=None):
 
-    df_daily_ipca = pd.read_csv('BRL Daily Inflation.csv')
+    df_daily_ipca = pd.read_csv('C:\\Users\\e-joaom\\Downloads\\api\\Inflation-Rate-Update-API\\BRL Daily Inflation.csv')
+    df_daily_ipca['Date'] = pd.to_datetime(df_daily_ipca['Date'])
 
-    df = pd.DataFrame(columns=['Date', 'Value'])
-    df['Date'] = dates
-    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
-    df['Value'] = values
+    present_date = df_daily_ipca['Date'].max() if present_date == None else present_date
 
-    present_date = pd.to_datetime(present_date, format='%d/%m/%Y')
+    df = pd.DataFrame({'Date': [dates], 
+                       'Value': [values]})
+    
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    present_date = pd.to_datetime(present_date)
     df_daily_ipca_to_present_date = df_daily_ipca[df_daily_ipca['Date'] <= present_date]
-    df_daily_ipca_to_present_date['IPCA Value Accumulated'] = (1 + df_daily_ipca_to_present_date['IPCA Value'])[::-1].cumprod()[::-1] - 1
+    df_daily_ipca_to_present_date['IPCA Value Accumulated (%)'] = (1 + df_daily_ipca_to_present_date['IPCA Value'])[::-1].cumprod()[::-1] - 1
 
     if present_date <= df_daily_ipca_to_present_date['Date'].max():
         df = df[(df['Date'] >= df_daily_ipca_to_present_date['Date'].min()) & (df['Date'] < present_date)]
@@ -19,6 +22,9 @@ def adjust_BRL_inflation(dates:str, values:float, present_date:str=df_daily_ipca
         return f'The present date must be less than or equal to {df_daily_ipca_to_present_date['Date'].max().date()}'
 
     df = df.merge(df_daily_ipca_to_present_date, on='Date', how='left')
-    df[f'Adjusted Value - {present_date}'] = df['Value'] * (1 + df['IPCA Value Accumulated'])
+    df[f'Adjusted Value - {present_date.date()}'] = round(df['Value'] * (1 + df['IPCA Value Accumulated (%)']), 2)
+
+    df['IPCA Value Accumulated (%)'] = round(df['IPCA Value Accumulated (%)'] * 100, 2)
+    df = df.drop(columns=['IPCA Value'])
 
     return df
