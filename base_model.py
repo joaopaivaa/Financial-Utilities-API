e45ad213bd774b1bc269c30f8f5e71cf0a254e21
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-19 09:58:04 -0300

    Update requests

--- base_model.py
+++ base_model.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel, Field
 
-class InflationRequest(BaseModel):
-    dates: str = Field(description='Dates', default=None)
-    values: float = Field(description='Values', default=None)
+class AdjustInflationRequest(BaseModel):
+    dates: list[str] = Field(description='Dates', default=None)
+    values: list[float] = Field(description='Values', default=None)
+    currency: str = Field(description='Values', default=None)
     present_date: str = Field(description='Present Date', default=None)
 
-class CurrencyConversionRequest(BaseModel):
-    dates: str = Field(description='Dates', default=None)
-    values: float = Field(description='Values', default=None)
+class CurrencyRateConversionRequest(BaseModel):
+    dates: list[str] = Field(description='Dates', default=None)
+    values: list[float] = Field(description='Values', default=None)
     original_currency: str = Field(description='Original Currency', default=None)
