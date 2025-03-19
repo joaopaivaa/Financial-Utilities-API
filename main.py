4e38d1c34d44634f77adb3a7c802d24efbefb86c
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-19 09:57:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-import end_points
+from routers import adjust_inflation, currency_rate_conversion
 
-app = FastAPI(title='Inflation Rate Adjust API',
+app = FastAPI(title='Financial Utilities API',
               version='0.0.1')
 
-app.include_router(end_points.router, tags=['Recipe Finder'])
+app.include_router(adjust_inflation.router, tags=['Adjust Inflation Route'])
+app.include_router(currency_rate_conversion.router, tags=['Currency Rate Conversion Route'])
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="127.0.0.1", port=8000)
