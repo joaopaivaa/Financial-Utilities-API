2b7642e90a6026eea3db06887d74125b4a2acbca
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-19 09:03:59 -0300

    add CurrencyConversionRequest to base_model.py

--- base_model.py
+++ base_model.py
@@ -4,3 +4,8 @@ class InflationRequest(BaseModel):
     dates: str = Field(description='Dates', default=None)
     values: float = Field(description='Values', default=None)
     present_date: str = Field(description='Present Date', default=None)
+
+class CurrencyConversionRequest(BaseModel):
+    dates: str = Field(description='Dates', default=None)
+    values: float = Field(description='Values', default=None)
+    original_currency: str = Field(description='Original Currency', default=None)
