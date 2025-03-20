75bf8bf5e02bb6c18450e9279596eece9f145ccc
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-20 14:25:10 -0300

    add interest and amortization routes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
-from routers import adjust_inflation, currency_rate_conversion
+from routers import adjust_inflation, currency_rate_conversion, interest, amortization
 
 app = FastAPI(title='Financial Utilities API',
               version='0.0.1')
 
 app.include_router(adjust_inflation.router, tags=['Adjust Inflation Route'])
 app.include_router(currency_rate_conversion.router, tags=['Currency Rate Conversion Route'])
+app.include_router(interest.router, tags=['Simple and Compound Interest Calculation Route'])
+app.include_router(amortization.router, tags=['French and SAC Amortization Calculation Route'])
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="127.0.0.1", port=8000)
