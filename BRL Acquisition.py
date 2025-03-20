89c677da7a4640ae09a0e2d364be6a8e024e3ea6
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-20 12:46:24 -0300

    fix leap day (29/february)

--- BRL Acquisition.py
+++ BRL Acquisition.py
@@ -1,32 +1,38 @@
 import requests
 import pandas as pd
 from datetime import datetime
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json"
 response = requests.get(url)
 data = response.json()
 
-df_ipca = pd.DataFrame(data)
-df_ipca.rename(columns={'data': 'Date', 'valor': 'Inflation Value'}, inplace=True)
-df_ipca['Date'] = pd.to_datetime(df_ipca['Date'], format='%d/%m/%Y')
-df_ipca['Inflation Value'] = pd.to_numeric(df_ipca['Inflation Value']) / 100
-df_ipca['Month Number'] = [int(datetime.strftime(df_ipca['Date'][index], format='%m')) for index in df_ipca.index]
+df = pd.DataFrame(data)
+df.rename(columns={'data': 'Date', 'valor': 'Inflation Value'}, inplace=True)
+df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+df['Inflation Value'] = pd.to_numeric(df['Inflation Value']) / 100
+df['Month Number'] = [int(datetime.strftime(df['Date'][index], format='%m')) for index in df.index]
 
-df_ipca = df_ipca[df_ipca['Date'] >= pd.to_datetime('01/07/1994', format='%d/%m/%Y')].reset_index(drop=True)
+df = df[df['Date'] >= pd.to_datetime('01/07/1994', format='%d/%m/%Y')].reset_index(drop=True)
 
-df_days_per_month = pd.read_csv("months_days.csv")
+df_days_per_month = pd.read_csv(BASE_DIR + "\months_days.csv")
 
-df_ipca = df_ipca.merge(df_days_per_month, on='Month Number', how='left')
+df = df.merge(df_days_per_month, on='Month Number', how='left')
 
-df_ipca[(df_ipca['Month'] == 'February') &
-        (df_ipca['Date'].dt.year.astype(int) % 4 == 0) &
-        ((df_ipca['Date'].dt.year.astype(int) % 100 != 0) | (df_ipca['Date'].dt.year.astype(int) % 400 == 0))]['Number of Days'] = 29
+df.loc[
+    (df['Month'] == 'February') &
+    (df['Date'].dt.year % 4 == 0) &
+    ((df['Date'].dt.year % 100 != 0) | (df['Date'].dt.year % 400 == 0)),
+    'Number of Days'
+] = 29
 
 df_daily_ipca = pd.DataFrame(columns=['Date', 'Inflation Value'])
-for i in df_ipca.index:
+for i in df.index:
     df_date = pd.DataFrame(columns=['Date', 'Inflation Value'])
-    df_date['Date'] = pd.date_range(start=df_ipca['Date'].values[i], periods=df_ipca['Number of Days'].values[i])
-    df_date['Inflation Value'] = ((1 + df_ipca['Inflation Value'].values[i])**(1/df_ipca['Number of Days'].values[i])) - 1
+    df_date['Date'] = pd.date_range(start=df['Date'].values[i], periods=df['Number of Days'].values[i])
+    df_date['Inflation Value'] = ((1 + df['Inflation Value'].values[i])**(1/df['Number of Days'].values[i])) - 1
     df_daily_ipca = pd.concat([df_daily_ipca, df_date], ignore_index=True)
 
-df_daily_ipca.to_csv('BRL Daily Inflation.csv', index=False)
+df_daily_ipca.to_csv(BASE_DIR + '\BRL Daily Inflation.csv', index=False)
