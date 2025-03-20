78d65211c0fceb92ca0d68d4ab70c1e8b5b5bd6f
João Vitor de Paiva Marcotti <50598946+joaopaivaa@users.noreply.github.com>
2025-03-20 09:58:08 -0300

    Update and rename BRL - data cleaning.py to BRL Acquisition.py

--- BRL Acquisition.py
+++ BRL Acquisition.py
@@ -7,9 +7,9 @@ response = requests.get(url)
 data = response.json()
 
 df_ipca = pd.DataFrame(data)
-df_ipca.rename(columns={'data': 'Date', 'valor': 'IPCA Value'}, inplace=True)
+df_ipca.rename(columns={'data': 'Date', 'valor': 'Inflation Value'}, inplace=True)
 df_ipca['Date'] = pd.to_datetime(df_ipca['Date'], format='%d/%m/%Y')
-df_ipca['IPCA Value'] = pd.to_numeric(df_ipca['IPCA Value']) / 100
+df_ipca['Inflation Value'] = pd.to_numeric(df_ipca['Inflation Value']) / 100
 df_ipca['Month Number'] = [int(datetime.strftime(df_ipca['Date'][index], format='%m')) for index in df_ipca.index]
 
 df_ipca = df_ipca[df_ipca['Date'] >= pd.to_datetime('01/07/1994', format='%d/%m/%Y')].reset_index(drop=True)
@@ -22,11 +22,11 @@ df_ipca[(df_ipca['Month'] == 'February') &
         (df_ipca['Date'].dt.year.astype(int) % 4 == 0) &
         ((df_ipca['Date'].dt.year.astype(int) % 100 != 0) | (df_ipca['Date'].dt.year.astype(int) % 400 == 0))]['Number of Days'] = 29
 
-df_daily_ipca = pd.DataFrame(columns=['Date', 'IPCA Value'])
+df_daily_ipca = pd.DataFrame(columns=['Date', 'Inflation Value'])
 for i in df_ipca.index:
-    df_date = pd.DataFrame(columns=['Date', 'IPCA Value'])
+    df_date = pd.DataFrame(columns=['Date', 'Inflation Value'])
     df_date['Date'] = pd.date_range(start=df_ipca['Date'].values[i], periods=df_ipca['Number of Days'].values[i])
-    df_date['IPCA Value'] = ((1 + df_ipca['IPCA Value'].values[i])**(1/df_ipca['Number of Days'].values[i])) - 1
+    df_date['Inflation Value'] = ((1 + df_ipca['Inflation Value'].values[i])**(1/df_ipca['Number of Days'].values[i])) - 1
     df_daily_ipca = pd.concat([df_daily_ipca, df_date], ignore_index=True)
 
 df_daily_ipca.to_csv('BRL Daily Inflation.csv', index=False)
