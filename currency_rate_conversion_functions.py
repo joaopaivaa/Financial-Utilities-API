2d2c349fc88d1d4c50ffb4d460574b50f46627f6
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-03-22 00:00:00 -0300

    fix currencies names

--- currency_rate_conversion_functions.py
+++ currency_rate_conversion_functions.py
@@ -7,23 +7,23 @@ def currency_rate_conversion(dates: List[str], values: List[float], original_cur
 
     df = pd.DataFrame({'Date': dates, original_currency: values})
     
-    if original_currency is 'Great Britain Pound':
+    if original_currency is 'GBP':
         df = df.merge(df_currencies, on='Date', how='left')
-        df['Dollar'] = df[original_currency] * df['GBP-USD']
-        df['Euro'] = df[original_currency] * df['GBP-EUR']
+        df['USD'] = df[original_currency] * df['GBP-USD']
+        df['EUR'] = df[original_currency] * df['GBP-EUR']
 
-    elif original_currency is 'Euro':
+    elif original_currency is 'EUR':
         df = df.merge(df_currencies, on='Date', how='left')
-        df['Dollar'] = df[original_currency] * df['EUR-USD']
-        df['Great Britain Pound'] = df[original_currency] * df['EUR-GBP']
+        df['USD'] = df[original_currency] * df['EUR-USD']
+        df['GBP'] = df[original_currency] * df['EUR-GBP']
 
-    elif original_currency is 'Dollar':
+    elif original_currency is 'USD':
         df = df.merge(df_currencies, on='Date', how='left')
-        df['Great Britain pound'] = df[original_currency] * df['USD-GBP']
-        df['Euro'] = df[original_currency] * df['USD-EUR']
+        df['GBP'] = df[original_currency] * df['USD-GBP']
+        df['EUR'] = df[original_currency] * df['USD-EUR']
 
     else:
-        return 'Please, select a currency between Euro, Dollar and Great Britain Pound'
+        return 'Please, select a currency between EUR, USD and GBP'
     
     df = df.drop(columns=['EUR-USD', 'EUR-GBP', 'GBP-USD', 'GBP-EUR', 'USD-GBP', 'USD-EUR'])
     
