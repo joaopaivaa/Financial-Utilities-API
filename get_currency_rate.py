431dab52e041186083c00cd2e327bda4abd114f4
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-03-22 00:00:46 -0300

    export currencies data and update it to run just the last day and concat

--- get_currency_rate.py
+++ get_currency_rate.py
@@ -1,26 +1,33 @@
 import yfinance as yf
 import pandas as pd
 import os
+from datetime import datetime, timedelta
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-eur_usd = yf.download(['EURUSA=X'], period='max')
+previous_df = pd.read_csv(os.path.join(BASE_DIR, 'Currencies Rate.csv'), sep=';')
 
-eur_gbp = yf.download(['EURGBP=X'], period='max')
+previous_date = previous_df.iloc[-1]['Date']
+actual_date = str(pd.to_datetime(datetime.now()).date())
 
-gbp_usd = yf.download(['GBPUSD=X'], period='max')
+if not (actual_date in previous_df['Date'].values):
 
-gbp_eur = yf.download(['GBPEUR=X'], period='max')
+    eur_usd = yf.download(['EURUSD=X'], start=previous_date, end=actual_date)[['Close']]
 
-usd_gbp = yf.download(['GBP=X'], period='max')
+    eur_gbp = yf.download(['EURGBP=X'], start=previous_date, end=actual_date)[['Close']]
 
-usd_eur = yf.download(['EUR=X'], period='max')
+    gbp_usd = yf.download(['GBPUSD=X'], start=previous_date, end=actual_date)[['Close']]
 
-df_currencies = pd.DataFrame({'EUR-USD': eur_usd,
-                              'EUR-GBP': eur_gbp,
-                              'GBP-USD': gbp_usd,
-                              'GBP-EUR': gbp_eur,
-                              'USD-GBP': usd_gbp,
-                              'USD-EUR': usd_eur})
+    gbp_eur = yf.download(['GBPEUR=X'], start=previous_date, end=actual_date)[['Close']]
 
-df_currencies.to_csv(os.path.join(BASE_DIR, 'Currencies Rate.csv'), sep=';', index=False)
+    usd_gbp = yf.download(['GBP=X'], start=previous_date, end=actual_date)[['Close']]
+
+    usd_eur = yf.download(['EUR=X'], start=previous_date, end=actual_date)[['Close']]
+
+    df_currencies = pd.concat([eur_usd, eur_gbp, gbp_usd, gbp_eur, usd_gbp, usd_eur], axis=1, join="outer")['Close']
+    df_currencies = df_currencies.reset_index().rename(columns={'index': 'Date'})
+    df_currencies['Date'] = df_currencies['Date'].astype(str)
+
+    df_currencies = pd.concat([previous_df, df_currencies], ignore_index=True)
+
+    df_currencies.to_csv(os.path.join(BASE_DIR, 'Currencies Rate.csv'), sep=';', index=True)
