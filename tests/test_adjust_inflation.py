a262e777bd6de9064bed1f66d6588f93fb5625c4
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-03-22 23:26:03 -0300

    Dollar to USD and fix adjust inflation test

--- tests/test_adjust_inflation.py
+++ tests/test_adjust_inflation.py
@@ -9,7 +9,7 @@ def test_BRL_adjust_inflation_route():
     data = {
         'dates': ['2020/01/01', '2020/02/01', '2020/03/01', '2020/04/01', '2020/05/01'],
         'values': [100, 150, 200, 250, 300],
-        'currency': 'EUR',
+        'currency': 'BRL',
         'present_date': '2025/01/01'
     }
     response = client.post('/adjust_inflation', json=data)
@@ -42,12 +42,12 @@ def test_GBP_adjust_inflation_route():
     assert 'Accumulated Inflation (%)' in response_json
     assert len(response_json['Accumulated Inflation (%)']) > 0
 
-def test_Dollar_adjust_inflation_route():
+def test_USD_adjust_inflation_route():
 
     data = {
         'dates': ['2020/01/01', '2020/02/01', '2020/03/01', '2020/04/01', '2020/05/01'],
         'values': [100, 150, 200, 250, 300],
-        'currency': 'Dollar',
+        'currency': 'USD',
         'present_date': '2025/01/01'
     }
     response = client.post('/adjust_inflation', json=data)
