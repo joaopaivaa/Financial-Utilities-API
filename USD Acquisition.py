efb40d5fe898d11c6dc6ffc1de409ef5e2848b6a
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-04-24 16:58:21 -0300

    USD acquisition to monthly inflation

--- USD Acquisition.py
+++ USD Acquisition.py
@@ -59,25 +59,10 @@ df = df[["Date", "CPI Value", "Monthly Inflation"]]
 
 df['Monthly Inflation'] = pd.to_numeric(df['Monthly Inflation']) / 100
 df['Month Number'] = [int(datetime.strftime(df['Date'][index], format='%m')) for index in df.index]
+df['Year Number'] = [int(datetime.strftime(df['Date'][index], format='%Y')) for index in df.index]
 
 df = df[df['Date'] >= pd.to_datetime('01/01/1967', format='%d/%m/%Y')].reset_index(drop=True)
 
-df_days_per_month = pd.read_csv(BASE_DIR + '\databases\months_days.csv')
+df.drop(columns=['CPI Value'], inplace=True)
 
-df = df.merge(df_days_per_month, on='Month Number', how='left')
-
-df.loc[
-    (df['Month'] == 'February') &
-    (df['Date'].dt.year % 4 == 0) &
-    ((df['Date'].dt.year % 100 != 0) | (df['Date'].dt.year % 400 == 0)),
-    'Number of Days'
-] = 29
-
-df_daily_inflation = pd.DataFrame(columns=['Date', 'Daily Inflation'])
-for i in df.index:
-    df_date = pd.DataFrame(columns=['Date', 'Daily Inflation'])
-    df_date['Date'] = pd.date_range(start=df['Date'].values[i], periods=df['Number of Days'].values[i])
-    df_date['Daily Inflation'] = ((1 + df['Monthly Inflation'].values[i])**(1/df['Number of Days'].values[i])) - 1
-    df_daily_inflation = pd.concat([df_daily_inflation, df_date], ignore_index=True)
-
-df_daily_inflation.to_csv(BASE_DIR + '\databases\\USD Daily Inflation.csv', index=False, encoding='utf-8')
+df.to_csv(BASE_DIR + '\databases\\USD Monthly Inflation.csv', index=False, encoding='utf-8')
