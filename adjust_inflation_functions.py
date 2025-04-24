8de9bfd694f792eca0052caba3ae98928e0b8009
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-04-24 16:58:54 -0300

    Updates to adjust monthly inflation

--- adjust_inflation_functions.py
+++ adjust_inflation_functions.py
@@ -1,42 +1,46 @@
 import pandas as pd
 from typing import Literal
 import os
+from datetime import datetime
 
 def adjust_inflation(dates:list[str], values:list[float], currency:Literal['BRL','GBP','USD'], present_date:str=None):
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     if currency == 'BRL':
-        df_daily_ipca = pd.read_csv(rf'{BASE_DIR}/databases/BRL Daily Inflation.csv')
+        df_monthly_ipca = pd.read_csv(rf'{BASE_DIR}/databases/BRL Monthly Inflation.csv')
 
     elif currency == 'USD':
-        df_daily_ipca = pd.read_csv(rf'{BASE_DIR}/databases/USD Daily Inflation.csv')
+        df_monthly_ipca = pd.read_csv(rf'{BASE_DIR}/databases/USD Monthly Inflation.csv')
 
     elif currency == 'GBP':
-        df_daily_ipca = pd.read_csv(rf'{BASE_DIR}/databases/GBP Daily Inflation.csv')
+        df_monthly_ipca = pd.read_csv(rf'{BASE_DIR}/databases/GBP Monthly Inflation.csv')
 
-    df_daily_ipca['Date'] = pd.to_datetime(df_daily_ipca['Date'])
+    df_monthly_ipca['Date'] = pd.to_datetime(df_monthly_ipca['Date'])
 
-    present_date = df_daily_ipca['Date'].max() if present_date == None else present_date
+    present_date = df_monthly_ipca['Date'].max() if present_date == None else present_date
 
     df = pd.DataFrame({'Date': dates, 
                        'Value': values})
     
     df['Date'] = pd.to_datetime(df['Date'])
+    df['Month Number'] = [int(datetime.strftime(df['Date'][index], format='%m')) for index in df.index]
+    df['Year Number'] = [int(datetime.strftime(df['Date'][index], format='%Y')) for index in df.index]
 
     present_date = pd.to_datetime(present_date)
-    df_daily_ipca_to_present_date = df_daily_ipca[df_daily_ipca['Date'] <= present_date].copy()
-    df_daily_ipca_to_present_date['Accumulated Inflation (%)'] = (1 + df_daily_ipca_to_present_date['Daily Inflation'])[::-1].cumprod()[::-1] - 1
+    df_monthly_ipca_to_present_date = df_monthly_ipca[df_monthly_ipca['Date'] <= present_date].copy()
+    df_monthly_ipca_to_present_date['Accumulated Inflation'] = (1 + df_monthly_ipca_to_present_date['Monthly Inflation'])[::-1].cumprod()[::-1] - 1
 
-    if present_date <= df_daily_ipca_to_present_date['Date'].max():
-        df = df[(df['Date'] >= df_daily_ipca_to_present_date['Date'].min()) & (df['Date'] < present_date)]
+    if present_date <= df_monthly_ipca_to_present_date['Date'].max():
+        df = df[(df['Date'] >= df_monthly_ipca_to_present_date['Date'].min()) & (df['Date'] < present_date)]
     else:
-        return f"The present date must be less than or equal to {df_daily_ipca_to_present_date['Date'].max().date()}"
+        return f"The present date must be less than or equal to {df_monthly_ipca_to_present_date['Date'].max().date()}"
 
-    df = df.merge(df_daily_ipca_to_present_date, on='Date', how='left')
-    df[f'Adjusted Value - {present_date.date()}'] = round(df['Value'] * (1 + df['Accumulated Inflation (%)']), 2)
+    df = df.merge(df_monthly_ipca_to_present_date, on=['Month Number','Year Number'], how='left')
+    df.columns = df.columns.str.replace('Date_x', 'Date', regex=False)
+    df[f'Adjusted Value - {present_date.date()}'] = round(df['Value'] * (1 + df['Accumulated Inflation']), 2)
 
-    df['Accumulated Inflation (%)'] = round(df['Accumulated Inflation (%)'] * 100, 2)
-    df = df.drop(columns=['Daily Inflation'])
+    df['Accumulated Inflation (%)'] = round(df['Accumulated Inflation'] * 100, 2)
+    df = df.drop(columns=['Monthly Inflation', 'Month Number', 'Year Number', 'Accumulated Inflation', 'Date_y'], axis=1)
 
     return df
