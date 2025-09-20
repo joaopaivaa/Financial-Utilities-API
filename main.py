3a79264eb09ae159e87f26c7d00b7a59eccb4e11
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-09-20 12:30:01 -0300

    update databases

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
-from routers import adjust_inflation, currency_rate_conversion, interest, amortization
+from routers import inflation_adjustment, currency_rate_conversion, interest, amortization
 
 app = FastAPI(title='Financial Utilities API',
               version='0.0.1')
 
-app.include_router(adjust_inflation.router, tags=['Adjust Inflation Route'])
+app.include_router(inflation_adjustment.router, tags=['Adjust Inflation Route'])
 app.include_router(currency_rate_conversion.router, tags=['Currency Rate Conversion Route'])
 app.include_router(interest.router, tags=['Simple and Compound Interest Calculation Route'])
 app.include_router(amortization.router, tags=['French and SAC Amortization Calculation Route'])
