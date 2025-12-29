6d64f811ff4003dab36ba6d29a05782e17c4c747
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-12-28 22:17:04 -0300

    adjusted inflation_adjustment function to adjust until the most recent date

--- inflation_adjustment_functions.py
+++ inflation_adjustment_functions.py
@@ -16,6 +16,9 @@ def inflation_adjustment(dates:list[str], values:list[float], currency:Literal['
     elif currency == 'GBP':
         df_monthly_ipca = pd.read_csv(rf'{BASE_DIR}/databases/GBP Monthly Inflation.csv')
 
+    else:
+        return "Currency not supported. Please select 'BRL', 'GBP', or 'USD'."
+
     df_monthly_ipca['Date'] = pd.to_datetime(df_monthly_ipca['Date'])
 
     present_date = df_monthly_ipca['Date'].max() if present_date == None else present_date
@@ -31,14 +34,14 @@ def inflation_adjustment(dates:list[str], values:list[float], currency:Literal['
     df_monthly_ipca_to_present_date = df_monthly_ipca[df_monthly_ipca['Date'] <= present_date].copy()
     df_monthly_ipca_to_present_date['Accumulated Inflation'] = (1 + df_monthly_ipca_to_present_date['Monthly Inflation'])[::-1].cumprod()[::-1] - 1
 
-    if present_date <= df_monthly_ipca_to_present_date['Date'].max():
-        df = df[(df['Date'] >= df_monthly_ipca_to_present_date['Date'].min()) & (df['Date'] < present_date)]
+    if present_date <= df_monthly_ipca_to_present_date['Date'].max() + pd.DateOffset(months=1):
+        df = df[(df['Date'] >= df_monthly_ipca_to_present_date['Date'].min()) & (df['Date'] <= present_date)]
     else:
         return f"The present date must be less than or equal to {df_monthly_ipca_to_present_date['Date'].max().date()}"
 
     df = df.merge(df_monthly_ipca_to_present_date, on=['Month Number','Year Number'], how='left')
     df.columns = df.columns.str.replace('Date_x', 'Date', regex=False)
-    df[f'Adjusted Value - {present_date.date()}'] = round(df['Value'] * (1 + df['Accumulated Inflation']), 2)
+    df[f'Adjusted Value - {present_date.month}/{present_date.year}'] = round(df['Value'] * (1 + df['Accumulated Inflation']), 2)
 
     df['Accumulated Inflation (%)'] = round(df['Accumulated Inflation'] * 100, 2)
     df = df.drop(columns=['Monthly Inflation', 'Month Number', 'Year Number', 'Accumulated Inflation', 'Date_y'], axis=1)
